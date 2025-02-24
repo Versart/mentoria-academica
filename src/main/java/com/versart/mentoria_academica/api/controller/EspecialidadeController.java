4c7b33570ce162ab4cde7dc19f77f1328b1ce38b
Versart <wesleyversart@gmail.com>
2025-02-24 20:54:04 -0300

    feat: Adicionando endpoints de CRUD para especialidades
    Adiciona endpoints de CRUD para especialidades

--- src/main/java/com/versart/mentoria_academica/api/controller/EspecialidadeController.java
+++ src/main/java/com/versart/mentoria_academica/api/controller/EspecialidadeController.java
@@ -5,12 +5,13 @@ import com.versart.mentoria_academica.api.model.EspecialidadeResponse;
 import com.versart.mentoria_academica.domain.service.EspecialidadeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/especialidades")
@@ -23,5 +24,26 @@ public class EspecialidadeController {
     public ResponseEntity<EspecialidadeResponse> salvarEspecialidade(@RequestBody @Valid EspecialidadeRequest especialidadeRequest) {
 
         return new ResponseEntity<>(especialidadeService.salvarEspecialidade(especialidadeRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<EspecialidadeResponse>> listarEspecialidades(Pageable pageable) {
+        return ResponseEntity.ok(especialidadeService.listarEspecialidades(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EspecialidadeResponse> buscarEspecialidadePorId(@PathVariable UUID id) {
+        return ResponseEntity.ok(especialidadeService.buscarEspecialidadePorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EspecialidadeResponse> alterarEspecialidade(@PathVariable UUID id, @RequestBody EspecialidadeRequest especialidadeRequest) {
+        return ResponseEntity.ok(especialidadeService.alterarEspecialidade(id,especialidadeRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarEspecialidade(@PathVariable UUID id) {
+        especialidadeService.deletarEspecialidade(id);
+        return ResponseEntity.noContent().build();
     }
 }
