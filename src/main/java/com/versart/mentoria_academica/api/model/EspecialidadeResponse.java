feba6ced3b1c1ecfccd82f2a9da9681b13da8f4f
Versart <wesleyversart@gmail.com>
2025-02-24 21:07:52 -0300

    feat: Adicionando ID em EspecialidadeResponse
    Adiciona oD na EspecialidadeResponse ,ue é retornada pela API

--- src/main/java/com/versart/mentoria_academica/api/model/EspecialidadeResponse.java
+++ src/main/java/com/versart/mentoria_academica/api/model/EspecialidadeResponse.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class EspecialidadeResponse {
+    private UUID id;
 
     private String nome;
 }
