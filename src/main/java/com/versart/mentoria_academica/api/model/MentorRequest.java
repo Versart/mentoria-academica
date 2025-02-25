37534fc297c2f84ba409eef3d9b48b5edc951915
Versart <wesleyversart@gmail.com>
2025-02-24 21:24:47 -0300

    feat: Adicionando especialidades no MentorRequest
    Adiciona um Set de Especialidades no MentorRequest, classe usada para requisições do tipo POST e PUT em mentores

--- src/main/java/com/versart/mentoria_academica/api/model/MentorRequest.java
+++ src/main/java/com/versart/mentoria_academica/api/model/MentorRequest.java
@@ -1,6 +1,10 @@
 package com.versart.mentoria_academica.api.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
-public record MentorRequest(@NotBlank String codigo, @NotBlank String departamento, @NotBlank String nomeCompleto) {
+import java.util.Set;
+
+public record MentorRequest(@NotBlank String codigo, @NotBlank String departamentoNome, @NotBlank String nomeCompleto, @NotNull @NotEmpty Set<String> especialidades) {
 }
