2008ea833e8ac51d432ed4ea669e006569c38526
Versart <wesleyversart@gmail.com>
2025-02-27 20:06:43 -0300

    feat: Adicionando métodos rCRUD eestantes m EspecialidadeService
    Adiciona outros métodos CRUD além de salvar em EspecialideService

--- src/main/java/com/versart/mentoria_academica/domain/service/EspecialidadeService.java
+++ src/main/java/com/versart/mentoria_academica/domain/service/EspecialidadeService.java
@@ -3,12 +3,17 @@ package com.versart.mentoria_academica.domain.service;
 import com.versart.mentoria_academica.api.model.EspecialidadeRequest;
 import com.versart.mentoria_academica.api.model.EspecialidadeResponse;
 import com.versart.mentoria_academica.domain.exception.DadoUnicoDuplicadoException;
+import com.versart.mentoria_academica.domain.exception.NaoEncontradoException;
 import com.versart.mentoria_academica.domain.model.Especialidade;
 import com.versart.mentoria_academica.domain.repository.EspecialidadeRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +21,7 @@ public class EspecialidadeService {
 
     private final EspecialidadeRepository especialidadeRepository;
 
+    @Transactional
     public EspecialidadeResponse salvarEspecialidade(EspecialidadeRequest especialidadeRequest) {
         if(especialidadeRepository.existsByNome(especialidadeRequest.nome())){
             throw new DadoUnicoDuplicadoException("Especialidade já cadastrada!");
@@ -26,5 +32,48 @@ public class EspecialidadeService {
         EspecialidadeResponse especialidadeResponse = new EspecialidadeResponse();
         BeanUtils.copyProperties(especialidadeSalva, especialidadeResponse);
         return especialidadeResponse;
+    }
+
+    public Page<EspecialidadeResponse> listarEspecialidades(Pageable pageable) {
+        Page<Especialidade> especialidades = especialidadeRepository.findAll(pageable);
+        return  especialidades.map(
+                especialidade -> {
+                    EspecialidadeResponse especialidadeResponse = new EspecialidadeResponse();
+                    BeanUtils.copyProperties(especialidade,especialidadeResponse);
+                    return  especialidadeResponse;
+                }
+        );
+    }
+
+    public EspecialidadeResponse buscarEspecialidadePorId(UUID id) {
+        return especialidadeRepository.findById(id).map(
+                especialidade -> {
+                    EspecialidadeResponse especialidadeResponse = new EspecialidadeResponse();
+                    BeanUtils.copyProperties(especialidade,especialidadeResponse);
+                    return especialidadeResponse;
+                }
+        ).orElseThrow(() -> new NaoEncontradoException("Especialidade não encontrada"));
+    }
+
+    @Transactional
+    public EspecialidadeResponse alterarEspecialidade(UUID id, EspecialidadeRequest especialidadeRequest) {
+        return especialidadeRepository.findById(id).map(
+            especialidade -> {
+                BeanUtils.copyProperties(especialidadeRequest,especialidade);
+                EspecialidadeResponse especialidadeResponse = new EspecialidadeResponse();
+                BeanUtils.copyProperties(especialidade, especialidadeResponse);
+                return especialidadeResponse;
+            }
+        ).orElseThrow(() -> new NaoEncontradoException("Especialidade não encontrada"));
+    }
+
+    @Transactional
+    public void deletarEspecialidade(UUID id) {
+        if(especialidadeRepository.existsById(id)){
+            especialidadeRepository.deleteById(id);
+        }
+        else{
+            throw new NaoEncontradoException("Especialidade não encontrada");
+        }
     }
 }
