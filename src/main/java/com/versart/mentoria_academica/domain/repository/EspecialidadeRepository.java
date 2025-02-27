5604714b3731d92f28588439129b4dd43154574a
Versart <wesleyversart@gmail.com>
2025-02-27 19:46:36 -0300

    feat: Adicionando método para buscar Especialidade por nome
    Adiciona método em EspecialidadeRespository que busca por nome e retorna um Optional

--- src/main/java/com/versart/mentoria_academica/domain/repository/EspecialidadeRepository.java
+++ src/main/java/com/versart/mentoria_academica/domain/repository/EspecialidadeRepository.java
@@ -3,9 +3,12 @@ package com.versart.mentoria_academica.domain.repository;
 import com.versart.mentoria_academica.domain.model.Especialidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EspecialidadeRepository extends JpaRepository<Especialidade, UUID> {
 
     boolean existsByNome(String nome);
+
+    Optional<Especialidade> findByNome(String nome);
 }
