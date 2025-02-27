f9311051c64e3adf1d9da9bd4f69a0bcb83e2c6b
Versart <wesleyversart@gmail.com>
2025-02-27 19:50:20 -0300

    feat: Adicionando método para buscar Mentor por código
    Adiciona novo método em MentorRepository para buscar um mentor pelo código que retorna um Optional de Mentor

--- src/main/java/com/versart/mentoria_academica/domain/repository/MentorRepository.java
+++ src/main/java/com/versart/mentoria_academica/domain/repository/MentorRepository.java
@@ -3,10 +3,13 @@ package com.versart.mentoria_academica.domain.repository;
 import com.versart.mentoria_academica.domain.model.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MentorRepository extends JpaRepository<Mentor, UUID> {
 
 
     boolean existsByCodigo(String codigo);
+
+    Optional<Mentor> findByCodigo(String codigo);
 }
