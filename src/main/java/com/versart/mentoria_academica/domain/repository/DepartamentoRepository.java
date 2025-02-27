2390cf023704c1769f6ab2ad05973087b647e521
Versart <wesleyversart@gmail.com>
2025-02-27 19:37:37 -0300

    feat: Adicionando método para buscar por nome
    Adiciona num étodo para buscar por nome que retorna um optional

--- src/main/java/com/versart/mentoria_academica/domain/repository/DepartamentoRepository.java
+++ src/main/java/com/versart/mentoria_academica/domain/repository/DepartamentoRepository.java
@@ -3,8 +3,11 @@ package com.versart.mentoria_academica.domain.repository;
 import com.versart.mentoria_academica.domain.model.Departamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DepartamentoRepository extends JpaRepository<Departamento, UUID> {
     boolean existsByNome(String nome);
+
+    Optional<Departamento> findByNome(String nome);
 }
