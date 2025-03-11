1c30cbd1f4fe627aa7ff8461cfcd60984a4b13de
Versart <wesleyversart@gmail.com>
2025-03-10 23:49:07 -0300

    refactor: Utilizando MapStruct
    Utiliza biblioteca MapStruct para fazer mapeamento de objetos de requisição para objetos de modelo

--- src/main/java/com/versart/mentoria_academica/domain/service/MentorService.java
+++ src/main/java/com/versart/mentoria_academica/domain/service/MentorService.java
@@ -1,24 +1,20 @@
 package com.versart.mentoria_academica.domain.service;
 
+import com.versart.mentoria_academica.api.mapper.MentorMapper;
 import com.versart.mentoria_academica.api.model.MentorRequest;
 import com.versart.mentoria_academica.api.model.MentorResponse;
 import com.versart.mentoria_academica.domain.exception.DadoUnicoDuplicadoException;
 import com.versart.mentoria_academica.domain.exception.NaoEncontradoException;
-import com.versart.mentoria_academica.domain.model.Departamento;
-import com.versart.mentoria_academica.domain.model.Especialidade;
 import com.versart.mentoria_academica.domain.model.Mentor;
 import com.versart.mentoria_academica.domain.repository.DepartamentoRepository;
 import com.versart.mentoria_academica.domain.repository.EspecialidadeRepository;
 import com.versart.mentoria_academica.domain.repository.MentorRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -31,28 +27,16 @@ public class MentorService {
 
     private final EspecialidadeRepository especialidadeRepository;
 
+    private final MentorMapper mentorMapper;
+
     @Transactional
     public MentorResponse salvarMentor(MentorRequest mentorRequest) {
-        if(mentorRepository.existsByCodigo(mentorRequest.codigo())){
+        if(verificarSeCodigoExiste(mentorRequest.codigo())){
             throw new DadoUnicoDuplicadoException("Código já utilizado");
         }
-        Departamento departamento = departamentoRepository.findByNome(mentorRequest.departamentoNome()).orElseThrow(
-                () -> new NaoEncontradoException("Departamento não encontrado")
-        );
-        Set<Especialidade> especialidades = new HashSet<>();
-        mentorRequest.especialidades().forEach(
-                especialidade -> {
-                    especialidades.add(especialidadeRepository.findByNome(especialidade).orElseThrow(
-                            () -> new NaoEncontradoException("Especialidade não encontrada")));
-                }
-        );
-        var mentor = new Mentor();
-        mentor.setDepartamento(departamento);
-        mentor.setEspecialidades(especialidades);
-        BeanUtils.copyProperties(mentorRequest, mentor);
+        Mentor mentor = mentorMapper.toMentor(mentorRequest,especialidadeRepository,departamentoRepository);
         Mentor mentorSalvo = mentorRepository.save(mentor);
-        MentorResponse mentorResponse = new MentorResponse();
-        BeanUtils.copyProperties(mentorSalvo,mentorResponse);
+        MentorResponse mentorResponse = mentorMapper.toMentorResponse(mentorSalvo);
         return mentorResponse;
     }
 
@@ -60,8 +44,7 @@ public class MentorService {
         Page<Mentor> pageMentores = mentorRepository.findAll(pageable);
         return pageMentores.map( mentor -> {
             MentorResponse mentorResponse = new MentorResponse();
-            BeanUtils.copyProperties(mentor,mentorResponse);
-            return mentorResponse;
+            return mentorMapper.toMentorResponse(mentor);
         });
 
     }
@@ -70,20 +53,23 @@ public class MentorService {
         return mentorRepository.findById(id).map(
                 mentor -> {
                     MentorResponse mentorResponse = new MentorResponse();
-                    BeanUtils.copyProperties(mentor, mentorResponse);
-                    return mentorResponse;
+                    return mentorMapper.toMentorResponse(mentor);
                 }
         ).orElseThrow(() -> new NaoEncontradoException("Mentor não encontrado"));
     }
 
     @Transactional
     public MentorResponse alterarMentor(UUID id, MentorRequest mentorRequest) {
+        if(verificarSeCodigoExiste(mentorRequest.codigo())){
+            throw new DadoUnicoDuplicadoException("Código já utilizado");
+        }
         return mentorRepository.findById(id).map(
                 mentor -> {
-                    BeanUtils.copyProperties(mentorRequest, mentor);
+                    Mentor mentorAlterado = mentorMapper.toMentor(mentorRequest,especialidadeRepository,departamentoRepository);
+                    mentorAlterado.setId(id);
+                    mentorRepository.save(mentorAlterado);
                     MentorResponse mentorResponse = new MentorResponse();
-                    BeanUtils.copyProperties(mentor,mentorResponse);
-                    return mentorResponse;
+                    return mentorMapper.toMentorResponse(mentorAlterado);
                 }
         ).orElseThrow(() -> new NaoEncontradoException("Mentor não encontrado"));
     }
@@ -96,5 +82,9 @@ public class MentorService {
         else{
             throw new NaoEncontradoException("Mentor não encontrado");
         }
+    }
+
+    private boolean verificarSeCodigoExiste(String codigo) {
+        return mentorRepository.existsByCodigo(codigo);
     }
 }
