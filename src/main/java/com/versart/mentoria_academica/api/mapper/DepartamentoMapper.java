de1c89100810cb7be3aa3b5f98db845b87b60806
Versart <wesleyversart@gmail.com>
2025-03-17 21:09:53 -0300

    feat: Adicionando método para mapeamento
    Adiciona método de mapeamento da classe DepartamentoMapper de  Departamento para DepartamentoResponse

--- src/main/java/com/versart/mentoria_academica/api/mapper/DepartamentoMapper.java
+++ src/main/java/com/versart/mentoria_academica/api/mapper/DepartamentoMapper.java
@@ -1,17 +1,21 @@
 package com.versart.mentoria_academica.api.mapper;
 
 import com.versart.mentoria_academica.api.model.DepartamentoRequest;
+import com.versart.mentoria_academica.api.model.DepartamentoResponse;
 import com.versart.mentoria_academica.domain.model.Departamento;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",uses = MentorMapper.class)
 public interface DepartamentoMapper {
 
     DepartamentoMapper INSTANCE = Mappers.getMapper(DepartamentoMapper.class);
 
+
     Departamento toDepartamento(DepartamentoRequest departamentoRequest);
 
+
+    DepartamentoResponse toDepartamentoResponse(Departamento departamento);
 
 
 }
