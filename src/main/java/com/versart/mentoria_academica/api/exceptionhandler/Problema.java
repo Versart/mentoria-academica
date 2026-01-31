5c701f6cc81c88e638c13d98eec9c20afc138a83
Versart <wesleyversart@gmail.com>
2026-01-31 16:53:33 -0300

    fix: Resolvendo problema da data/hora não formatada
    Resolve problemas de data/hora mostrando a data/hora formatada de forma correta e não mostrando apenas os segundos

--- src/main/java/com/versart/mentoria_academica/api/exceptionhandler/Problema.java
+++ src/main/java/com/versart/mentoria_academica/api/exceptionhandler/Problema.java
@@ -1,5 +1,6 @@
 package com.versart.mentoria_academica.api.exceptionhandler;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
@@ -17,7 +18,7 @@ public class Problema {
     private int statusCode;
 
     private String messagem;
-
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ssXXX")
     private OffsetDateTime dataHora;
 
     private List<Campo> campos;
