8d8d81ffd9887772de1b115cfa84453e24a7d7ca
Versart <wesleyversart@gmail.com>
2026-02-15 23:35:20 -0300

    fix: Resolvendo bug SpringDoc
    Resolve bug spring doc

--- src/main/java/com/versart/mentoria_academica/configuration/SpringDocConfig.java
+++ src/main/java/com/versart/mentoria_academica/configuration/SpringDocConfig.java
@@ -6,13 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SpringDocConfig {
 
+    
      @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
+        return new OpenAPI().addServersItem(new Server().url("https://mentoria-academica-production.up.railway.app/"))
           .components(new Components()
           .addSecuritySchemes("bearer-key",
             new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
