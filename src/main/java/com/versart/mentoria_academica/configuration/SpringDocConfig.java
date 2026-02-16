44e9d3a126d4c9a3f4b4f04a5f6a0a4d267b9345
Versart <wesleyversart@gmail.com>
2026-02-16 06:00:48 -0300

    fix: Resolvendo problema de falha no swagger
    Resolve falha do swagger

--- src/main/java/com/versart/mentoria_academica/configuration/SpringDocConfig.java
+++ src/main/java/com/versart/mentoria_academica/configuration/SpringDocConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SpringDocConfig {
@@ -14,6 +15,7 @@ public class SpringDocConfig {
      @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+          .addServersItem(new Server().url("https://mentoria-academica-676779291748.southamerica-east1.run.app/"))
           .components(new Components()
           .addSecuritySchemes("bearer-key",
             new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
