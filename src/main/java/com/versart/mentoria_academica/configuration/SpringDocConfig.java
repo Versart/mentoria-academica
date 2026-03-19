2eebc0f50d6254bf260287869ef3a2e21d2b3403
Versart <wesleyversart@gmail.com>
2026-03-18 21:48:36 -0300

    refactor: Alterando para variavel de ambiente
    Altera server do springdoc para buscar de uma variavel de ambiente

--- src/main/java/com/versart/mentoria_academica/configuration/SpringDocConfig.java
+++ src/main/java/com/versart/mentoria_academica/configuration/SpringDocConfig.java
@@ -1,5 +1,6 @@
 package com.versart.mentoria_academica.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +12,13 @@ import io.swagger.v3.oas.models.servers.Server;
 @Configuration
 public class SpringDocConfig {
 
+    @Value("${url.document}")
+    private String serverUrl;
     
-     @Bean
+    @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-          .addServersItem(new Server().url("https://mentoria-academica-676779291748.southamerica-east1.run.app/"))
+          .addServersItem(new Server().url(serverUrl))
           .components(new Components()
           .addSecuritySchemes("bearer-key",
             new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
