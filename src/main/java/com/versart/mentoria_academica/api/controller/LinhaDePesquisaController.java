c98e380740069bd7c1f780eb2fdc75160bc7f88a
Versart <wesleyversart@gmail.com>
2026-06-13 00:19:39 -0300

    refactor: Renomeando métodos de LinhaDePesquisa
    Renomeia métodos do LinhaPesquisaController e Service

--- src/main/java/com/versart/mentoria_academica/api/controller/LinhaDePesquisaController.java
+++ src/main/java/com/versart/mentoria_academica/api/controller/LinhaDePesquisaController.java
@@ -26,31 +26,31 @@ public class LinhaDePesquisaController {
     private final LinhaDePesquisaService linhaDePesquisaService;
 
     @PostMapping
-    public ResponseEntity<LinhaDePesquisaResponse> salvarEspecialidade(@RequestBody @Valid LinhaDePesquisaRequest linhaDePesquisaRequest) {
+    public ResponseEntity<LinhaDePesquisaResponse> salvarLinhaDePesquisa(@RequestBody @Valid LinhaDePesquisaRequest linhaDePesquisaRequest) {
         log.info("Requisição recebida para criar uma linha de pesquisa");
         return new ResponseEntity<>(linhaDePesquisaService.salvarLinhaDePesquisa(linhaDePesquisaRequest), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<Page<LinhaDePesquisaResponse>> listarEspecialidades(Pageable pageable) {
+    public ResponseEntity<Page<LinhaDePesquisaResponse>> listarLinhasDePesquisa(Pageable pageable) {
         log.info("Requisição recebida para buscar todas as linhas de pesquisa");
         return ResponseEntity.ok(linhaDePesquisaService.listarLinhasDePesquisa(pageable));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<LinhaDePesquisaResponse> buscarEspecialidadePorId(@PathVariable UUID id) {
+    public ResponseEntity<LinhaDePesquisaResponse> buscarLinhaDePesquisaPorId(@PathVariable UUID id) {
         log.info("Requisição recebida para buscar a linha de pesquisa com o id {}", id);
         return ResponseEntity.ok(linhaDePesquisaService.buscarLinhaDePesquisaPorId(id));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<LinhaDePesquisaResponse> alterarEspecialidade(@PathVariable UUID id, @RequestBody LinhaDePesquisaRequest linhaDePesquisaRequest) {
+    public ResponseEntity<LinhaDePesquisaResponse> alterarLinhaDePesquisa(@PathVariable UUID id, @RequestBody LinhaDePesquisaRequest linhaDePesquisaRequest) {
         log.info("Requisição recebida para alterar a linha de pesquisa com o id {}", id);
-        return ResponseEntity.ok(linhaDePesquisaService.alterarEspecialidade(id,linhaDePesquisaRequest));
+        return ResponseEntity.ok(linhaDePesquisaService.alterarLinhaDePesquisa(id,linhaDePesquisaRequest));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletarEspecialidade(@PathVariable UUID id) {
+    public ResponseEntity<Void> deletarLinhaDepesquisa(@PathVariable UUID id) {
         log.info("Requisição recebida para remover a linha de pesquisa com o id {}", id);
         linhaDePesquisaService.deletarLinhaDePesquisa(id);
         return ResponseEntity.noContent().build();
