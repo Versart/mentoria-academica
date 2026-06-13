371e7f826eec1e04ea7b3db4b94c9f4d19efe533
Versart <wesleyversart@gmail.com>
2026-06-13 00:20:48 -0300

    feat: Adicionando builder nos DTOs
    Adiciona builder nos DTOs de Departamento

--- src/main/java/com/versart/mentoria_academica/api/model/DepartamentoRequest.java
+++ src/main/java/com/versart/mentoria_academica/api/model/DepartamentoRequest.java
@@ -1,6 +1,8 @@
 package com.versart.mentoria_academica.api.model;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record DepartamentoRequest(@NotBlank String nome) {
 }
