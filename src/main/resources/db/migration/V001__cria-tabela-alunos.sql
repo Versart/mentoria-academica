a3e1230c57d91004ee69309ce42a01a803b2dcee
Versart <wesleyversart@gmail.com>
2026-06-21 16:22:27 -0300

    refactor: Removendo alunos
    Remove alunos pois não estava sendo utilizado, antes da integração com o SUAP iria ser preciso se cadastrar e por isso a tabela

--- src/main/resources/db/migration/V001__cria-tabela-alunos.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists alunos(
-    id varchar(255) primary key,
-    codigo varchar(20) not null,
-    curso varchar(100) not null,
-    nome_completo varchar(255) not null
-)
--- src/main/resources/db/migration/V004__altera-tabela-alunos-codigo.sql
+++
@@ -1,1 +0,0 @@
-alter table alunos add unique (codigo)
